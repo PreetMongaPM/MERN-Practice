bc79e0e1398e5987ccc99d14cc17f04d0757a188
Preet Monga <preetmongaclersiy@gmail.com>
2025-12-25 19:06:05 +0530

    Developed two counter buttons and a multipler of these counters and a reset button to toggle them back to 0

---
+++ Javascript/alert_prompt_confirm.js
@@ -0,0 +1,17 @@
+//can't run on node as window functions
+
+let userName = prompt('Enter your name','Anonymous');
+alert(`Hi, ${userName}. Have a good day!`);
+
+let isGay = confirm("Are you gay?")
+if(isGay)
+{
+    alert("I knew it");
+}
+else{
+    while(isGay != true)
+    {
+        isGay = confirm("Tu hai tujhe pta nhi hai");
+    }
+    alert("I knew it");
+}
