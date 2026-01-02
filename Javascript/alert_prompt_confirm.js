0449ef34f6a548dd76d34466078dfe6bac81d220
PreetMongaPM <preetmongapm1@gmail.com>
2026-01-02 22:17:11 +0530

    github merged

--- Javascript/alert_prompt_confirm.js
+++ Javascript/alert_prompt_confirm.js
@@ -1,16 +1,14 @@
 //can't run on node as window functions
 
-let userName = prompt('Enter your name','Anonymous');
-alert(`Hi, ${userName}. Have a good day!`);
+// let userName = prompt('Enter your name','Anonymous');
+// alert(`Hi, ${userName}. Have a good day!`);
 
 let isGay = confirm("Are you gay?")
-if(isGay)
-{
+if (isGay) {
     alert("I knew it");
 }
-else{
-    while(isGay != true)
-    {
+else {
+    while (isGay != true) {
         isGay = confirm("Tu hai tujhe pta nhi hai");
     }
     alert("I knew it");
